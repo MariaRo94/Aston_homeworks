ebfa88b318fcc7f1dc10dfe91d839b58936a1386
Maria <mashascharer@mail.ru>
2024-04-18 23:35:27 +0300

    msg mycode

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       int [] array = new int [10];
+
+
+
+    }
+   public static void createArray(int [] array){
+       for (int i = 0; i <array.length ; i++) {
+
+
+       }
+   }
+}
